d58f7501cf5f6976e92c89209254129d3e492a66
Grégoire LE CORRE <lecorregregoire@gmail.com>
2024-04-15 20:48:26 +0200

    Basic ptitgit init

--- src/cmd/init.cpp
+++ src/cmd/init.cpp
@@ -1,8 +1,42 @@
-#include "init.h"
 #include <iostream>
+#include <filesystem>
+#include <vector>
+#include "init.h"
+
+/* In order to handle file manipulation, we use the filesystem library introduced in C++17*/
+
+PtiteGitInit::PtiteGitInit () {
+    if (std::filesystem::exists(".ptitgit")) {
+        std::cerr << "The current directory is already a git repos" << std::endl;
+    }
+    else {
+        try {
+            std::filesystem::create_directory(".ptitgit");
+        }
+        catch (const std::filesystem::filesystem_error& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
+    }
+}
+
+PtiteGitInit::PtiteGitInit (std::filesystem::path folderToInit) {
+    if(std::filesystem::exists(folderToInit / ".ptitgit")) {
+        std::cerr << "The given directory is already a git repos" << std::endl;
+    }
+    else {
+        try {
+            std::filesystem::create_directory(folderToInit / ".ptitgit");
+        }
+        catch (const std::filesystem::filesystem_error& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
+    }
+}
 
 void init() {
-    std::cout << "init" << std::endl;
+    PtiteGitInit myGitInit;
+}
 
-
+void init(std::filesystem::path folderToInit) {
+    PtiteGitInit myGitInit(folderToInit);
 }
--- src/cmd/init.h
+++ src/cmd/init.h
@@ -1,6 +1,19 @@
 #ifndef INIT_H
 #define INIT_H
 
+#include <filesystem>
+
 void init();
+void init(std::filesystem::path folderToInit);
+
+class PtiteGitInit {
+
+    public:
+        PtiteGitInit();
+        PtiteGitInit(std::filesystem::path folderToInit);
+
+    private:
+        std::filesystem::path myGitFolder;
+};
 
 #endif
