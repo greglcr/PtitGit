f7e2d116afad17a00cde263a2c53edb5c6a27147
UnknownMath2004 <quydangngo@gmail.com>
2024-05-14 16:22:10 +0200

    fix tag

--- src/parser.cpp
+++ src/parser.cpp
@@ -73,6 +73,7 @@ int main(int argc,char *argv[])  {
         if(argc == 2){
             std::map <fs::path, std::string> Y = ref_list({}, X, X.getWorkingFolder() / ".ptitgit" / "refs");
             for (const auto &[k, v] : Y) std::cout<<"The ref in "<<k<<" is "<<v<<".\n";
+            std::cout<<"???";
         }
         else if(strcmp(argv[2] , "-a") == 0){create = true; k = 3;}
         else{create = false; k = 2;}
