b988175cc0308657a2b445791b39985c8b68c105
UnknownMath2004 <quydangngo@gmail.com>
2024-05-14 16:18:15 +0200

    fix tag

--- src/parser.cpp
+++ src/parser.cpp
@@ -76,10 +76,9 @@ int main(int argc,char *argv[])  {
         }
         else if(strcmp(argv[2] , "-a") == 0){create = true; k = 3;}
         else{create = false; k = 2;}
-
+        std::cerr<<"Ref name missing"<<std::endl;
         if(argc < k + 1) std::cerr<<"Ref name missing"<<std::endl;
         else if(argc == k + 2){
-            std::cerr<<"Ref name miissing"<<std::endl;
             if(fs::exists(X.getWorkingFolder() / ".ptitgit" / "refs" / "tags" / argv[k+1])){
                 sha = ref_resolve(X, X.getWorkingFolder() / ".ptitgit" / "refs" / "tags" / argv[k+1]);
                 xyz.tag_create(X, argv[k], sha, "???", create);
