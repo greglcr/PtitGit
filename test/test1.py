7c5764f0d3a7dfd5991def7dcc6ee0e167137441
Vincent <j.vinc-sites@yahoo.com>
2024-05-22 17:31:45 +0200

    ajout du bug au test

--- test/test1.py
+++ test/test1.py
@@ -10,7 +10,18 @@ def test1():
 
     os.system('echo "42" > a') # create a file a
     os.system("../../petitGit add a")
-    #os.system("../../petitGit commit -m'bb'")
+    os.system("../../petitGit commit -m'2nd commit'")
+
+    os.system('mkdir b')
+    os.system('mkdir b/c')
+    os.system('echo "43" > b/c/d')
+    os.system('echo "44" > b/c/e')
+
+    os.system("../../petitGit add b/c/d")
+    os.system("../../petitGit commit -m'3rd commit'")
+
+
+
     
 
     # TODO
