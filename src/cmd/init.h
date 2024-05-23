9fd6c92a4d5ab4c7c10fefbb88917b1d2c740527
Grégoire LE CORRE <lecorregregoire@gmail.com>
2024-05-23 11:26:37 +0200

    Update folder creation

--- src/cmd/init.h
+++ src/cmd/init.h
@@ -8,8 +8,6 @@ const std::vector<std::string> FOLDERS_TO_CREATE = {
             ".ptitgit",
             ".ptitgit/index",
             ".ptitgit/objects",
-            ".ptitgit/objects/info",
-            ".ptitgit/objects/pack",
             ".ptitgit/refs",
             ".ptitgit/refs/heads",
             ".ptitgit/refs/tags"
